1070066315ad0fa9ef3e9f87ba9efdb36d78142d
pegeler <32426645+pegeler@users.noreply.github.com>
2023-11-19 03:19:55 -0500

    adding HTML parser

--- toc_gen.py
+++ toc_gen.py
@@ -3,15 +3,22 @@
 """
 Make Tables of Contents
 
-Generates a Table of Contents for Markdown documents.
-The table will be output to STDOUT.
+Generates a Table of Contents for Markdown and HTML documents. The table will
+be output to STDOUT or to file.
+
+This is non-production software that is somewhat fragile. User input is not
+carefully validated. Suggested usage is in interactive workflows when editing
+your own documents. Use at your own risk.
 """
+import abc
 import argparse
 from collections.abc import Iterator
 from collections.abc import Iterable
 from collections import Counter
 from dataclasses import dataclass
 from html import escape
+from html.parser import HTMLParser
+import os.path
 import re
 import sys
 
@@ -25,47 +32,64 @@ class TocEntry:
     link: str
 
 
-class BaseTocGenerator:
+class BaseSimpleDocumentParser(abc.ABC):
 
-    def __init__(self, infile, indent=4, outfile=None):
-        self.infile = infile
-        self.indent = indent
-        self.outfile = outfile
-
-    def write(self):
-        s = self.generateString()
-        if self.outfile:
-            with open(self.outfile, 'wt') as fh:
-                fh.write(s + '\n')
-        else:
-            print(s)
-
-    def generateString(self) -> str:
+    @abc.abstractmethod
+    def parseFile(self, infile) -> list[TocEntry]:
         raise NotImplementedError
 
 
-class MarkdownTocGenerator(BaseTocGenerator):
+class SimpleHtmlParser(HTMLParser, BaseSimpleDocumentParser):
+    headings: list[TocEntry]
+    _depth: int
+    _heading: str
+    _link: str
+
+    RE_HEADING = re.compile(r'^h(\d+)$', re.I)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.headings = []
+        self._in_heading_tag = False
+        self._heading = ''
+
+    def handle_starttag(self, tag, attrs):
+        if not self._in_heading_tag and (h := self.RE_HEADING.match(tag)):
+            self._in_heading_tag = True
+            self._depth = int(h.group(1)) - 1
+            for attr in attrs:
+                if attr[0] == 'id':
+                    self._link = attr[1]
+            else:
+                self._link = ''
+
+    def handle_endtag(self, tag):
+        if self._in_heading_tag and self.RE_HEADING.match(tag):
+            self._in_heading_tag = False
+            self.headings.append(TocEntry(self._depth, self._heading, self._link))
+            self._heading = ''
+
+    def handle_data(self, data):
+        if self._in_heading_tag:
+            self._heading += data.replace('\n', '')
+
+    def parseFile(self, infile) -> list[TocEntry]:
+        with open(infile, 'rt') as fh:
+            self.feed(fh.read())
+        return self.headings
+
+
+class SimpleMarkdownParser(BaseSimpleDocumentParser):
     FENCE = '```'
     RE_CAPTURE = re.compile('^(#+)(.*)$')
     RE_HTML_COMMENT = re.compile(r'<!--.*?-->', flags=re.S)  # Non-greedy match
     RE_SPECIALS = re.compile(r'''[!@#$%^&*()+;:'"\[\]{}|\\<>,./?`~]''')
 
-    def __init__(self, infile='README.md', indent=4, outfile=None):
-        super().__init__(infile, indent, outfile)
-        self.headings = self._parseFile(infile)
-
-    def generateString(self) -> str:
-        lines = ['## Table of Contents', '']
-        for heading in self.headings:
-            pad = heading.depth * self.indent * " "
-            lines.append(f'{pad}* [{heading.heading}](#{escape(heading.link)})')
-        return '\n'.join(lines)
-
     def _open_with_stripped_html_comments(self, filename: str) -> Iterator[str]:
         with open(filename) as f:
             yield from self.RE_HTML_COMMENT.sub('', f.read()).split('\n')
 
-    def _parseFile(self, infile) -> list[TocEntry]:
+    def parseFile(self, infile) -> list[TocEntry]:
         in_fence = False
         links = Counter()
         headings = []
@@ -96,28 +120,72 @@ class MarkdownTocGenerator(BaseTocGenerator):
         return headings
 
 
+class BaseTocGenerator(abc.ABC):
+    PARSERS = {
+        '.md': SimpleMarkdownParser().parseFile,
+        '.html': SimpleHtmlParser().parseFile,
+    }
+
+    def __init__(self, infile, indent=4, outfile=None):
+        self.infile = infile
+        self.indent = indent
+        self.outfile = outfile
+
+        _, ext = os.path.splitext(infile.lower())
+        try:
+            parser = self.PARSERS[ext]
+        except KeyError:
+            raise ValueError(f'Infile format {ext} not recognized')
+
+        self.headings = parser(infile)
+
+    def write(self):
+        s = self.generateString()
+        if self.outfile:
+            with open(self.outfile, 'wt') as fh:
+                fh.write(s + '\n')
+        else:
+            print(s)
+
+    @abc.abstractmethod
+    def generateString(self) -> str:
+        raise NotImplementedError
+
+
+class MarkdownTocGenerator(BaseTocGenerator):
+
+    def generateString(self) -> str:
+        lines = ['## Table of Contents', '']
+        for heading in self.headings:
+            pad = heading.depth * self.indent * " "
+            lines.append(f'{pad}* [{heading.heading}](#{escape(heading.link)})')
+        return '\n'.join(lines)
+
+
 class HtmlTagGenerator:
     def __init__(self, tags: Iterable[str]):
         self.__dict__.update(**{t: self._htmlTagGenerator(t) for t in tags})
 
     @staticmethod
     def _htmlTagGenerator(tag):
-        def html_tag(content):
-            return f'<{tag}>' + content + f'</{tag}>'
+        def html_tag(content, attrs=None):
+            attr_gen = (f'{k}="{v}"' for k, v in attrs or ())
+            return f'<{tag} {" ".join(attr_gen)}>' + content + f'</{tag}>'
         return html_tag
 
 
 class HtmlTocGenerator(BaseTocGenerator):
-    # TODO: complete implementation
 
-    def __init__(self, infile, indent=4, outfile=None):
-        super().__init__(infile, indent, outfile)
-        self.html_tags = HtmlTagGenerator(['h2', 'ul', 'li'])
+    def generateString(self) -> str:
         ...
+
+    _wrapInTag = HtmlTagGenerator(['h2', 'ul', 'li', 'a'])
 
 
 def parse_args(argv=None):
-    p = argparse.ArgumentParser(description=__doc__)
+    p = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
     p.add_argument(
         '-i', '--indent',
         dest='indent',
@@ -125,12 +193,11 @@ def parse_args(argv=None):
         default=4,
         help='The width of indentation.')
     p.add_argument(
-        # TODO: derive from extension?
         '-f', '--format',
         dest='format',
         default='markdown',
         choices=['markdown', 'html'],
-        help='The format of the document. (default: %(default)s)')
+        help='The output format. (default: %(default)s)')
     p.add_argument(
         '-o', '--outfile',
         dest='outfile',
