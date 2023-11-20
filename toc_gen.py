fb7eedaeca80cb94eea705d549c2b7484297ed56
pegeler <32426645+pegeler@users.noreply.github.com>
2023-11-20 15:52:40 -0500

    adding docstrings and small bugfixes

--- toc_gen.py
+++ toc_gen.py
@@ -3,8 +3,13 @@
 """
 Make Tables of Contents
 
-Generates a Table of Contents for Markdown and HTML documents. The table will
-be output to STDOUT or to file.
+Generates a Table of Contents for Markdown and HTML documents. Input and output
+formats can be mixed and matched. The table can be output to STDOUT or written
+to file.
+
+It is also extensible to any additional format you might want to add by
+subclassing the abstract base classes for parsing input files and generating
+output tables.
 
 This is non-production software that is somewhat fragile. User input is not
 carefully validated. Suggested usage is in interactive workflows when editing
@@ -22,8 +27,12 @@ from dataclasses import dataclass
 from html import escape
 from html.parser import HTMLParser
 from itertools import groupby
+from typing import Callable
+from typing import Optional
 
 __version__ = '1.0.0'
+
+HtmlTagAttrs = list[tuple[str, str]]
 
 
 @dataclass
@@ -34,14 +43,30 @@ class TocEntry:
 
 
 class BaseSimpleDocumentParser(abc.ABC):
+    """
+    Abstract base class for parsing document headings into a collection of
+    entries. A subclass of the BaseTocGenerator can use those entries to
+    generate a table of contents string.
+    """
 
     @abc.abstractmethod
     def parseFile(self, infile) -> list[TocEntry]:
+        """
+        Parse a file to create a list of TocEntries
+
+        :param str infile: The file to parse.
+        """
         raise NotImplementedError
 
 
 class SimpleHtmlParser(HTMLParser, BaseSimpleDocumentParser):
-    headings: list[TocEntry]
+    """
+    A parser that will generate a list of TOC entries from heading nodes,
+    (_e. g._, '<h2>Foo</h2>', '<h3>Bar</h3>') in an HTML documet.
+
+    :ivar entries: A list of TOC entries from the most recently parsed file.
+    """
+    entries: list[TocEntry]
     _depth: int
     _heading: str
     _link: str
@@ -50,7 +75,13 @@ class SimpleHtmlParser(HTMLParser, BaseSimpleDocumentParser):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.headings = []
+        self._reset()
+
+    def _reset(self):
+        self.entries = []
+        self._resetTagVariables()
+
+    def _resetTagVariables(self):
         self._in_heading_tag = False
         self._heading = ''
 
@@ -61,26 +92,33 @@ class SimpleHtmlParser(HTMLParser, BaseSimpleDocumentParser):
             for attr in attrs:
                 if attr[0] == 'id':
                     self._link = attr[1]
+                    break
             else:
                 self._link = ''
 
     def handle_endtag(self, tag):
         if self._in_heading_tag and self.RE_HEADING.match(tag):
-            self._in_heading_tag = False
-            self.headings.append(TocEntry(self._depth, self._heading, self._link))
-            self._heading = ''
+            self.entries.append(TocEntry(self._depth, self._heading, self._link))
+            self._resetTagVariables()
 
     def handle_data(self, data):
         if self._in_heading_tag:
             self._heading += data.replace('\n', '')
 
     def parseFile(self, infile) -> list[TocEntry]:
+        self._reset()
         with open(infile, 'rt') as fh:
             self.feed(fh.read())
-        return self.headings
+        return self.entries
 
 
 class SimpleMarkdownParser(BaseSimpleDocumentParser):
+    """
+    A parser that will create a list of TOC entries from heading lines
+    (_e. g._, '## Foo', '### Bar') in a Markdown document. Note that top level
+    headigs ('# Main title') will be ignored.
+    """
+
     FENCE = '```'
     RE_CAPTURE = re.compile('^(#+)(.*)$')
     RE_HTML_COMMENT = re.compile(r'<!--.*?-->', flags=re.S)  # Non-greedy match
@@ -93,7 +131,7 @@ class SimpleMarkdownParser(BaseSimpleDocumentParser):
     def parseFile(self, infile) -> list[TocEntry]:
         in_fence = False
         links = defaultdict(int)
-        headings = []
+        entries = []
 
         for line in self._openWithStrippedHtmlComments(infile):
             if line.startswith(self.FENCE):
@@ -116,13 +154,21 @@ class SimpleMarkdownParser(BaseSimpleDocumentParser):
             if n_links:
                 link = f'{link}-{str(n_links)}'
 
-            headings.append(TocEntry(depth, heading, link))
+            entries.append(TocEntry(depth, heading, escape(link)))
 
-        return headings
+        return entries
 
 
 class BaseTocGenerator(abc.ABC):
-    PARSERS = {
+    """
+    Abstract base class for generating a Table of Contents. Must be subclassed
+    to generate a Table of Contents string for the corresponding output format.
+
+    :cvar PARSERS: A dictionary of parsers for each input file format supported.
+            The key is the extension and the value should be a subclass of
+            BaseSimpleDocumentParser.
+    """
+    PARSERS: dict[str, Callable] = {
         '.md': SimpleMarkdownParser().parseFile,
         '.html': SimpleHtmlParser().parseFile,
     }
@@ -139,9 +185,12 @@ class BaseTocGenerator(abc.ABC):
         except KeyError:
             raise ValueError(f'Infile format {ext} not recognized')
 
-        self.headings = parser(infile)
+        self.entries = parser(infile)
 
     def write(self):
+        """
+        Write the Table of Contents to file or STDOUT.
+        """
         s = self.generateString()
         if self.outfile:
             with open(self.outfile, 'wt') as fh:
@@ -151,6 +200,9 @@ class BaseTocGenerator(abc.ABC):
 
     @abc.abstractmethod
     def generateString(self) -> str:
+        """
+        Generate a Table of Contents string corresponding to the format.
+        """
         raise NotImplementedError
 
 
@@ -158,10 +210,10 @@ class MarkdownTocGenerator(BaseTocGenerator):
 
     def generateString(self) -> str:
         lines = ['## Table of Contents', '']
-        for entry in self.headings:
+        for entry in self.entries:
             pad = entry.depth * self.indent_str
             if entry.link:
-                heading_str = f'[{entry.heading}](#{escape(entry.link)})'
+                heading_str = f'[{entry.heading}](#{entry.link})'
             else:
                 heading_str = entry.heading
             lines.append(f'{pad}* {heading_str}')
@@ -169,15 +221,38 @@ class MarkdownTocGenerator(BaseTocGenerator):
 
 
 class HtmlTagGenerator:
+    """
+    A variant of a SimpleNamespace that can be used to wrap strings in HTML
+    tags. For example,
+
+    >>> html_tags = HtmlTagGenerator(['h2', 'p', 'a'])
+    >>> html_tags.h2('This is a heading')
+    '<h2>This is a heading</h2>'
+    >>> mylink = html_tags.a("mylink", attrs=[("href", "#somewhere-in-doc")])
+    >>> html_tags.p(f'This is a link: {mylink}')
+    '<p>This is a link: <a href="#somewhere-in-doc">mylink</a></p>'
+
+    """
     # TODO: How do I type hint this? Modeled after SimpleNamespace.
     #   <https://docs.python.org/3/library/types.html#types.SimpleNamespace>
 
     def __init__(self, tags: Iterable[str]):
-        self.__dict__.update(**{t: self._htmlTagGenerator(t) for t in tags})
+        """
+        :param tags: The tags to be included. Accessed by dot notation.
+        """
+        self.__dict__.update(**{t: self.htmlTagGenerator(t) for t in tags})
 
     @staticmethod
-    def _htmlTagGenerator(tag):
+    def htmlTagGenerator(tag):
         def html_tag(content, *, attrs=None, newline=False, indent=None):
+            """
+            :param str content: The content to be placed inside the tag.
+            :param Optional[HtmlTagAttrs] attrs: The attributes to be inserted
+                    into the tag.
+            :param bool newline: Whether to add a newline between tags and
+                    content.
+            :param Optional[str] indent: Pad left the tags with this string.
+            """
             attr_str = ' ' + ' '.join(f'{k}="{v}"' for k, v in attrs) if attrs else ''
             newline_str = '\n' if newline else ''
             indent_str = indent or ''
@@ -195,9 +270,13 @@ class HtmlTocGenerator(BaseTocGenerator):
 
     def generateString(self) -> str:
         return (self._wrapInTag.h2('Table of Contents') + '\n' +
-                self._generateUlStr(self.headings, 0))
+                self._generateUlStr(self.entries, 0))
 
-    def _generateUlStr(self, entries: list[TocEntry], depth: int):
+    def _generateUlStr(self, entries: list[TocEntry], depth: int) -> str:
+        """
+        Generate a string containing potentially nested unordered list (<ul>)
+        nodes from a list of TOC entries.
+        """
         output = []
         groups = groupby(entries, lambda x: x.depth == depth)
         for is_same_depth, entry_group in groups:
@@ -211,6 +290,10 @@ class HtmlTocGenerator(BaseTocGenerator):
             '\n'.join(output), newline=True, indent=depth * self.indent_str)
 
     def _maybeWrapInLink(self, entry: TocEntry) -> str:
+        """
+        Create a string out of a TOC entry. If the entry contains a non-empty
+        link attribute, it will be wrapped in an anchor tag (<a>).
+        """
         if entry.link:
             attrs = [('href', f'#{entry.link}')]
             return self._wrapInTag.a(entry.heading, attrs=attrs)
